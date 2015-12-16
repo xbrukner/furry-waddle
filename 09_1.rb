d4efb89ccbdc4bd7a065db069eef01129687ad73
Tomas Brukner <tomas.brukner@gmail.com>
2015-12-16 22:09:28 +0100

    Add 9th day.

---
+++ 09_1.rb
@@ -0,0 +1,20 @@
+File.open("09_1.input", "r") do |file|
+  distances = Hash.new { |hash, e| hash[e] = Hash.new }
+
+  file.readlines.each do |l|
+    /([^ ]*) to ([^ ]*) = (\d+)/.match(l) do |m|
+      distances[m[1]][m[2]] = m[3].to_i
+      distances[m[2]][m[1]] = m[3].to_i
+    end
+  end
+
+  res = Float::MAX
+  distances.keys.permutation do |p|
+      c = 0
+      p.reduce { |from, to| c += distances[from][to]; to }
+      if c < res then
+        res = c
+      end
+  end
+  res
+end
