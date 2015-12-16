b7552d9f0e2435616d40fd2bb34777dea1dacf42
Tomas Brukner <tomas.brukner@gmail.com>
2015-12-16 20:55:24 +0100

    Add 7th day.

---
+++ 07_1.rb
@@ -0,0 +1,62 @@
+class Circuit
+  def initialize
+    @gates = Hash.new
+  end
+
+  def num_or_const(str)
+    if str =~ /\A\d+\Z/
+      str.to_i
+    else
+      if @gates[str].respond_to? :call
+        #Evaluate only once
+        @gates[str] = val = @gates[str].call()
+      else
+        @gates[str]
+      end
+    end
+  end
+
+  def handle(line)
+    matched = false
+    /([a-z]+|[0-9]+) \-\> ([a-z]+)/.match(line) do |m|
+      @gates[m[2]] = lambda { num_or_const(m[1]) }
+      matched = true
+    end
+    /([a-z]+|[0-9]+) AND ([a-z]+|[0-9]+) \-\> ([a-z]+)/.match(line) do |m|
+      @gates[m[3]] = lambda { num_or_const(m[1]) & num_or_const(m[2]) }
+      matched = true
+    end
+    /([a-z]+|[0-9]+) OR ([a-z]+|[0-9]+) \-\> ([a-z]+)/.match(line) do |m|
+      @gates[m[3]] = lambda { num_or_const(m[1]) | num_or_const(m[2]) }
+      matched = true
+    end
+    /NOT ([a-z]+|[0-9]+) \-\> ([a-z]+)/.match(line) do |m|
+      @gates[m[2]] = lambda { ~num_or_const(m[1]) }
+      matched = true
+    end
+    /([a-z]+|[0-9]+) LSHIFT ([0-9]+) \-\> ([a-z]+)/.match(line) do |m|
+      @gates[m[3]] = lambda { (num_or_const(m[1]) << m[2].to_i) & 65535 } #Do not go over 16 bits
+      matched = true
+    end
+    /([a-z]+|[0-9]+) RSHIFT ([0-9]+) \-\> ([a-z]+)/.match(line) do |m|
+      @gates[m[3]] = lambda { (num_or_const(m[1]) >> m[2].to_i) }
+      matched = true
+    end
+    if !matched then
+      puts "Line not matched: #{line}"
+      exit
+    end
+  end
+
+  def get(wire)
+    num_or_const(wire)
+  end
+end
+
+
+File.open('07_1.input', 'r') do |file|
+  c = Circuit.new
+  file.readlines
+    .each { |str| c.handle(str) }
+  puts "a: #{c.get('a')}"
+end
